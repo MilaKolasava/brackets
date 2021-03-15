a1163425d4dd3473d940d6931472a020222f86dc
MilaKolasava <kolasava.dev@yandex.by>
2021-03-15 21:37:01 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let br = [];
+    let pairSc = '';
+    for (i = 0; i < bracketsConfig.length; i++) {
+        pairSc = bracketsConfig[i][0] + bracketsConfig[i][1];
+        br.push(pairSc);
+        pairSc = "";
+    }
+    let count = 0;
+    do {
+        count = 0;
+        for (let i = 0; i < br.length; i++) {
+
+            if (str.indexOf(br[i]) != -1) {
+                str = str.replace(br[i], '');
+                count += 1;
+            }
+        }
+    } while (count != 0);
+    return (str.length == 0) ? true : false;
 }
